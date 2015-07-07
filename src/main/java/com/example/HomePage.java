08cb1c93a9f5f8771fe29af3d94521cadb1c008a
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2015-07-07 16:24:54 +0900

    add jquery, bootstrap for html

--- src/main/java/com/example/HomePage.java
+++ src/main/java/com/example/HomePage.java
@@ -1,18 +1,34 @@
 package com.example;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
+import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+	}
 
-		// TODO Add your page's components here
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(JavaScriptHeaderItem.forReference(
+				new WebjarsJavaScriptResourceReference("jquery/current/jquery.js")));
+		response.render(CssHeaderItem.forReference(
+				new WebjarsCssResourceReference("bootstrap/current/css/bootstrap.min.css")));
+		response.render(CssHeaderItem.forReference(
+				new WebjarsCssResourceReference("bootstrap/current/css/bootstrap-theme.min.css")));
+		response.render(JavaScriptHeaderItem.forReference(
+				new WebjarsJavaScriptResourceReference("bootstrap/current/js/bootstrap.min.js")));
+	}
 
-    }
 }
--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -3,9 +3,12 @@ package com.example;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
+import de.agilecoders.wicket.webjars.WicketWebjars;
+import de.agilecoders.wicket.webjars.settings.WebjarsSettings;
+
 /**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application without
+ * deploying, run the Start class.
  * 
  * @see com.example.Start#main(String[])
  */
@@ -15,8 +18,7 @@ public class WicketApplication extends WebApplication
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
 	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
+	public Class<? extends WebPage> getHomePage() {
 		return HomePage.class;
 	}
 
@@ -24,10 +26,10 @@ public class WicketApplication extends WebApplication
 	 * @see org.apache.wicket.Application#init()
 	 */
 	@Override
-	public void init()
-	{
+	public void init() {
 		super.init();
-
 		// add your configuration here
+		WebjarsSettings settings = new WebjarsSettings();
+		WicketWebjars.install(this, settings);
 	}
 }
