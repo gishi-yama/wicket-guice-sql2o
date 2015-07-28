940ac340a5d26bc35490914db86596d34a9b7a4a
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2015-07-28 18:26:44 +0900

    Use JQuery that is implemented in Wicket, Enable bootstrap

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.resource.DynamicJQueryResourceReference;
+import org.apache.wicket.util.crypt.CharEncoding;
 
 import de.agilecoders.wicket.webjars.WicketWebjars;
 import de.agilecoders.wicket.webjars.settings.WebjarsSettings;
@@ -14,22 +16,30 @@ import de.agilecoders.wicket.webjars.settings.WebjarsSettings;
  */
 public class WicketApplication extends WebApplication
 {
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends WebPage> getHomePage() {
-		return HomePage.class;
-	}
+  /**
+   * @see org.apache.wicket.Application#getHomePage()
+   */
+  @Override
+  public Class<? extends WebPage> getHomePage() {
+    return HomePage.class;
+  }
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init() {
-		super.init();
-		// add your configuration here
-		WebjarsSettings settings = new WebjarsSettings();
-		WicketWebjars.install(this, settings);
-	}
+  /**
+   * @see org.apache.wicket.Application#init()
+   */
+  @Override
+  public void init() {
+    super.init();
+    // サーバ・クライアント間のリクエスト・レスポンス時の文字エンコード
+    getRequestCycleSettings().setResponseRequestEncoding(CharEncoding.UTF_8);
+    // Wicketに取り込まれるHTMLファイルのエンコード
+    getMarkupSettings().setDefaultMarkupEncoding(CharEncoding.UTF_8);
+    // ブラウザにあわせたJQueryの読み込み
+    getJavaScriptLibrarySettings().setJQueryReference(new DynamicJQueryResourceReference());
+    // WebJarsの読み込み
+    WebjarsSettings settings = new WebjarsSettings();
+    WicketWebjars.install(this, settings);
+    // ページマウント
+    mountPage("Home", HomePage.class);
+  }
 }
